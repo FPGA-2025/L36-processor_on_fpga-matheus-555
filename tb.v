0df18ef516475b4ca6a9f087c7138d83edba3dba
Matheus de Alencar <sletezes@gmail.com>
2025-08-08 13:45:10 -0300

    refatorado

--- tb.v
+++ tb.v
@@ -3,15 +3,14 @@
 module tb();
 
 localparam QTDE_LINHAS_TESTE = 1;
-localparam QTDE_NIBBLE_TESTE = 2;
+localparam QTDE_BITS_TESTE = 8;
 
 reg clk = 0;
 reg rst_n;
 wire [7:0] leds;
 
-reg [QTDE_LINHAS_TESTE-1:0] file_data [0:QTDE_NIBBLE_TESTE-1];
+reg [QTDE_BITS_TESTE-1:0] file_data [0:QTDE_LINHAS_TESTE-1];
 reg [7:0] expected_leds;
-reg [1:0] test_case;
 
 always #1 clk = ~clk; // Clock generation
 
@@ -23,19 +22,9 @@ core_top #(
     .leds(leds)
 );
 
-integer i;
-reg [7:0] counter = 0;
-
 initial begin
     $dumpfile("saida.vcd");
     $dumpvars(0, tb);
-
-    $readmemh("teste.txt", file_data); // Read the memory file
-
-    test_case = file_data[0][0:0];
-    expected_leds = 8'hf;
-
-    // $display("Test case: %b, Expected LEDs: %b, Expected Memory: %h", test_case, expected_leds, expected_memory);
 
     rst_n = 0; // Reset the system
     #5;
@@ -43,7 +32,12 @@ initial begin
 
     #50; // wait for the end of the program
 
-    $display("Teste de escrita nos LEDS...");
+    $readmemh("test/teste0.txt", file_data); // Read the memory file
+
+    expected_leds = file_data[0][QTDE_BITS_TESTE-1:0];
+
+    $display("Expected LEDs: %h", expected_leds);
+
     if (leds !== expected_leds) begin
         $display("=== ERRO Escrita nos LEDS falhou: esperava %h, obtive %h", expected_leds, leds);
     end else begin
